f3ae8f9721f89cdb1e12fe300e9afa2c067c3525
Sifeizhai9630 <50127683+Sifeizhai9630@users.noreply.github.com>
2019-12-19 18:12:10 +0800

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping("/ab")
     String home() {
         return "Hello from GazGeek!";
     }
